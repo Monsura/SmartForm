ab396fc9d287c1578238d246a4f5be2ab0dfa8fe
cis101 <cis101@secs.iub.edu.bd>
2018-02-12 09:42:55 +0600

    assignment 7

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,31 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+	name: '',
+	email: ''
+};
+
+$('#start').click(function(){
+	console.log("start");
+	$('#welcome').hide();
+	$('#q1').show();
+});
+
+$('#name').change(function(event)
+{
+	console.log(event);
+	console.log($('name').val());
+});
+
+$('#q1next').click(function(){
+	if($('#name').val() && $('email').val()){
+	userData.name = $('#name').val();
+	userData.email = $('#email').val();
+	$('#q2').hide();
+	$('#q1').show();
+	}
+	else{
+		console.log('please enter name & email');
+	}
+
+	});
